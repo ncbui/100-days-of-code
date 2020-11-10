a4163c7c650aa1eb263824807cfcc7db06c82713
Cam T. Bui <ncbui@users.noreply.github.com>
2020-11-10 08:31:13 -0800

    Cleaned up notes

--- DSA/00-recursive-mean.js
+++ DSA/00-recursive-mean.js
@@ -20,7 +20,10 @@ class LinkedList {
     for (let val of vals) this.push(val);
   }
 
-  /** meanRecursive(): return the mean of all values in a linked list*/
+  /** meanRecursive(): return the mean of all values in a linked list
+   * 
+   * runtime: O(n), where n = items in list
+  */
 
   meanRecursive() {
     if (!this.length) return 0;
@@ -33,22 +36,6 @@ class LinkedList {
 
     return (head.val / this.length) + this._recursion(head.next);
   }
-  // /** meanRecursive(): return the mean of all values in a linked list
-  //  * 
-  //  * runtime: O(n), where n = items in list
-  // */
-
-  // meanRecursive() {
-  //   if (!this.length) return 0;
-
-  //   return this._recursion(this.head);
-  // }
-
-  // _recursion(head) {
-  //   if (head === null) return 0;
-
-  //   return (head.val / this.length) + this._recursion(head.next);
-  // }
 
   /** mean(): return an mean of all values in the list 
    * 
